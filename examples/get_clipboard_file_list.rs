50ffd7539a16faf2224a1ce50f8c4959a5e46bb1
Rolf Karp <rolf.karp@gmail.com>
2023-12-03 18:10:01 +0100

    Redesign clipboard functions

--- examples/get_clipboard_file_list.rs
+++ examples/get_clipboard_file_list.rs
@@ -1,14 +1,11 @@
 #![allow(clippy::uninlined_format_args)]
 
 use std::io;
-use winapi_easy::clipboard::Clipboard;
+use winapi_easy::clipboard;
 
 fn main() -> io::Result<()> {
-    Clipboard::new()?
-        .get_file_list()?
-        .into_iter()
-        .for_each(|name| {
-            println!("{:#?}", name);
-        });
+    clipboard::get_file_list()?.into_iter().for_each(|name| {
+        println!("{:#?}", name);
+    });
     Ok(())
 }
--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,6 +1,4 @@
-/*!
-Clipboard access.
-*/
+//! Clipboard access.
 
 use std::ffi::OsString;
 use std::io;
@@ -23,62 +21,72 @@ use crate::internal::{
     ReturnValue,
 };
 
-/// An opened Windows clipboard.
+/// Returns a list of file paths that have been copied to the clipboard.
 ///
-/// Will be closed again when dropped.
-pub struct Clipboard(());
+/// Will return `Err` if the clipboard cannot be accessed or does not contain files.
+pub fn get_file_list() -> io::Result<Vec<PathBuf>> {
+    let f = || {
+        let mut clipboard_data = {
+            let clipboard_data = unsafe { GetClipboardData(CF_HDROP.0.into()) }?;
+            GlobalLockedData::lock(clipboard_data)?
+        };
 
-impl Clipboard {
-    pub fn new() -> io::Result<Clipboard> {
-        unsafe {
-            OpenClipboard(None)
-                .if_null_get_last_error()
-                .map(|_| Clipboard(()))
-        }
-    }
-
-    /// Returns a list of file paths that have been copied to the clipboard.
-    ///
-    /// Will return `Err` if the clipboard cannot be accessed or does not contain files.
-    pub fn get_file_list(&self) -> io::Result<Vec<PathBuf>> {
-        unsafe {
-            let mut clipboard_data = {
-                let clipboard_data = GetClipboardData(CF_HDROP.0.into())?;
-                GlobalLockedData::lock(clipboard_data)?
-            };
-
-            let num_files = DragQueryFileW(HDROP(clipboard_data.ptr() as isize), u32::MAX, None);
-            let file_names: io::Result<Vec<PathBuf>> = (0..num_files)
-                .into_iter()
-                .map(|file_index| {
-                    let required_size =
-                        1 + DragQueryFileW(HDROP(clipboard_data.ptr() as isize), file_index, None)
-                            .if_null_to_error(|| io::ErrorKind::Other.into())?;
-                    let file_str_buf = {
-                        let mut buffer = vec![0; required_size as usize];
+        let num_files =
+            unsafe { DragQueryFileW(HDROP(clipboard_data.ptr() as isize), u32::MAX, None) };
+        let file_names: io::Result<Vec<PathBuf>> = (0..num_files)
+            .into_iter()
+            .map(|file_index| {
+                let required_size = unsafe {
+                    1 + DragQueryFileW(HDROP(clipboard_data.ptr() as isize), file_index, None)
+                }
+                .if_null_to_error(|| io::ErrorKind::Other.into())?;
+                let file_str_buf = {
+                    let mut buffer = vec![0; required_size as usize];
+                    unsafe {
                         DragQueryFileW(
                             HDROP(clipboard_data.ptr() as isize),
                             file_index,
                             Some(buffer.as_mut_slice()),
                         )
-                        .if_null_to_error(|| io::ErrorKind::Other.into())?;
-                        // Set length, remove terminating zero
-                        buffer.truncate(buffer.len() - 1);
-                        buffer
-                    };
-                    let os_string = OsString::from_wide(&file_str_buf);
-                    Ok(PathBuf::from(os_string))
-                })
-                .collect();
-            file_names
-        }
-    }
+                    }
+                    .if_null_to_error(|| io::ErrorKind::Other.into())?;
+                    // Set length, remove terminating zero
+                    buffer.truncate(buffer.len() - 1);
+                    buffer
+                };
+                let os_string = OsString::from_wide(&file_str_buf);
+                Ok(PathBuf::from(os_string))
+            })
+            .collect();
+        file_names
+    };
+    with_open_clipboard_do(f)
 }
 
-impl Drop for Clipboard {
-    fn drop(&mut self) {
-        unsafe {
-            CloseClipboard();
-        }
+fn with_open_clipboard_do<F, R>(f: F) -> io::Result<R>
+where
+    F: FnOnce() -> io::Result<R>,
+{
+    unsafe {
+        OpenClipboard(None).if_null_get_last_error()?;
+    }
+    let result = f();
+    unsafe {
+        CloseClipboard().if_null_get_last_error()?;
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::time::Duration;
+
+    #[test]
+    fn open_clipboard() -> io::Result<()> {
+        with_open_clipboard_do(|| {
+            std::thread::sleep(Duration::from_millis(0));
+            Ok(())
+        })
     }
 }
