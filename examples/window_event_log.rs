c962f729fbfc59017fddcc0242e212be6db56b1a
Rolf Karp <rolf.karp@gmail.com>
2025-05-13 15:16:28 +0200

    Rework hooking API
    - Allow processing hooks with a separate thread message loop
    - Allow hooking to be used with closures that are not `Send`

--- examples/window_event_log.rs
+++ examples/window_event_log.rs
@@ -15,27 +15,24 @@ fn main() -> io::Result<()> {
         caption: String,
         client_area: Rectangle,
     }
-    let hook_thread = std::thread::spawn(|| {
-        let callback = |message: WinEventMessage| {
-            match message.event_kind {
-                WinEventKind::ForegroundWindowChanged
-                | WinEventKind::WindowUnminimized
-                | WinEventKind::WindowMinimized
-                | WinEventKind::WindowMoveStart
-                | WinEventKind::WindowMoveEnd => {
-                    let window_handle = message.window_handle.unwrap();
-                    let message_content = MessageContent {
-                        kind: message.event_kind,
-                        caption: window_handle.get_caption_text(),
-                        client_area: window_handle.get_client_area_coords().unwrap(),
-                    };
-                    println!("{:#?}", message_content);
-                }
-                _ => (),
-            };
+    let callback = |message: WinEventMessage| {
+        match message.event_kind {
+            WinEventKind::ForegroundWindowChanged
+            | WinEventKind::WindowUnminimized
+            | WinEventKind::WindowMinimized
+            | WinEventKind::WindowMoveStart
+            | WinEventKind::WindowMoveEnd => {
+                let window_handle = message.window_handle.unwrap();
+                let message_content = MessageContent {
+                    kind: message.event_kind,
+                    caption: window_handle.get_caption_text(),
+                    client_area: window_handle.get_client_area_coords().unwrap(),
+                };
+                println!("{:#?}", message_content);
+            }
+            _ => (),
         };
-        WinEventHook::run_hook(callback)
-    });
-    hook_thread.join().unwrap()?;
+    };
+    WinEventHook::run_hook_loop(callback)?;
     Ok(())
 }
